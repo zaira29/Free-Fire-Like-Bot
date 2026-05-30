380fecbc85d0d7ea961d4fff88c0091b92b97295
zaira29 <86176362+zaira29@users.noreply.github.com>
2026-05-30 02:33:47 -0300

    Update API URL for call_api function

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def is_user_in_channel(user_id):
 
 
 def call_api(region, uid):
-    url = f"https://free-fire-like-bot-ob8t.onrender.com/like?uid={uid}&server_name={region}"
+    url = f"https://your-free-fire-like-api-domain/like?uid={uid}&server_name={region}"
     try:
         response = requests.get(url, timeout=20)
         if response.status_code != 200:
