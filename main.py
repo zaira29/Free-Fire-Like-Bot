38675e46c109910de9de6bb521105573f456b01b
zaira29 <86176362+zaira29@users.noreply.github.com>
2026-05-31 03:35:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def is_user_in_channel(user_id):
 
 
 def call_api(region, uid):
-    url = f"https://your-free-fire-like-api-domain/like?uid={uid}&server_name={region}"
+    url = f"https://no-permanent-like-api.vercel.app/like?uid={uid}&server_name={region}"
     try:
         response = requests.get(url, timeout=20)
         if response.status_code != 200:
